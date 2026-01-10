76f993fe45ea5b9829db1a7a53e3ef00a1e17cfc
Swayam <swayamkumar226@gmail.com>
2026-01-10 19:06:51 +0530

    header and status skeleton

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Header from "./components/Header";
+import Status from "./components/Status";
+
+function App() {
+  return (
+    <>
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background: `linear-gradient(45deg, #1a1a1a 0%, #003366 100%),
+        repeating-linear-gradient(
+          45deg,
+          rgba(0, 255, 255, 0.1) 0px,
+          rgba(0, 255, 255, 0.1) 20px,
+          rgba(0, 255, 0, 0.1) 20px,
+          rgba(0, 255, 0, 0.1) 40px
+        ),
+        radial-gradient(
+          circle at 50% 50%,
+          rgba(32, 196, 232, 0.3) 0%,
+          rgba(76, 201, 240, 0.1) 100%
+        )`,
+          backgroundBlendMode: "normal, overlay, overlay",
+          animation: "aurora 8s linear infinite",
+        }}
+      />
+      <Header />
+      <Status />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <nav className="relative z-10 pt-5 flex justify-center items-center flex-col">
+      <h1 className="text-4xl font-semibold p-5 text-white">
+        Assembly Endgame
+      </h1>
+      <p className="text-wrap text-gray-500 text-xl">
+        Guess the correct word under 6 turns to keep the programming world safe
+        from Assembly!!
+      </p>
+    </nav>
+  );
+}
