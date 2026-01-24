496ed33f626e838965450b205f34e6438cfe2522
Swayam <swayamkumar226@gmail.com>
2026-01-24 22:45:33 +0530

    heading responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <nav className="relative z-10 pt-5 flex justify-center items-center flex-col">
-      <h1 className="text-4xl font-semibold p-5 text-white">
+      <h1 className="text-xl font-semibold p-5 text-white md:text-4xl text-center">
         Beware of Assembly!!
       </h1>
-      <p className="text-wrap text-gray-500 text-xl max-w-xl text-center">
+      <p className="text-wrap text-gray-500 text-sm md:text-xl max-w-xl text-center">
         Guess the correct word under 8 turns to keep the programming world safe
         from Assembly!!
       </p>
